8fd8315651d1067ad38bf03d85881feda317011f
Michael Zhai <michael.zhai@mentemia.com>
2021-04-26 10:43:52 +1200

    Init project :tada:

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "useTabs": true,
+  "printWidth": 100,
+  "tabWidth": 2,
+  "singleQuote": true,
+  "trailingComma": "all",
+  "proseWrap": "always",
+	"bracketSpacing": true,
+  jsxBracketSameLine: true,
+};
